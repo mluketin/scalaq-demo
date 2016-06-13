467509b1bc7e045fed108e56c6661b290daf4355
Marin Luketin <marin.luketin@fer.hr>
2016-06-13 06:59:35 +0200

    Add Secured trait for restricting access to restricted routes

---
+++ app/controllers/Secured.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import play.api.mvc._
+
+trait Secured {
+
+  /**
+    * Retrieve the connected user's email
+    */
+  private def username(request: RequestHeader) = request.session.get("email")
+
+  /**
+    * Not authorized, forward to login
+    */
+  private def onUnauthorized(request: RequestHeader) = {
+    Results.Redirect(routes.Auth.login).flashing(
+      "restricted" -> "Restricted access")
+  }
+
+  /**
+    * Action for authenticated users.
+    */
+  def IsAuthenticated(f: => String => Request[AnyContent] => Result) = {
+    Security.Authenticated(username, onUnauthorized) { user =>
+      Action(request => f(user)(request))
+    }
+  }
+}

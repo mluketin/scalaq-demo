7faae4e5f13a900bc02ff9721f21c54e4870ff3f
Marin Luketin <marin.luketin@fer.hr>
2016-06-16 20:54:11 +0200

    Add crc32 to HashGenerator

--- app/models/HashGenerator.scala
+++ app/models/HashGenerator.scala
@@ -1,10 +1,20 @@
 package models
 
+import java.nio.charset.Charset
+import java.util.zip.{CRC32, Checksum}
+
 
 object HashGenerator {
 
   def getSha1(string: String): Array[Byte] = {
     val md = java.security.MessageDigest.getInstance("SHA-1")
     md.digest(string.getBytes("UTF-8"))
+  }
+
+  def getCrc32(string: String): Long = {
+    val bytes = string.getBytes()
+    val checksum: Checksum = new CRC32()
+    checksum.update(bytes, 0, bytes.length)
+    checksum.getValue()
   }
 }

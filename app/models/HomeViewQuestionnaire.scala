f588bd0c4d7e4c637c40a89ea71538fb3804d39a
Marin Luketin <marin.luketin@fer.hr>
2016-06-16 20:54:00 +0200

    Update HomeViewQuestionnaire, add more fields

--- app/models/HomeViewQuestionnaire.scala
+++ app/models/HomeViewQuestionnaire.scala
@@ -1,4 +1,16 @@
 package models
 
+import database.PersistanceAdapter
 
-case class HomeViewQuestionnaire(name: String, description: String)
+import scalaq.persistence.QOwnerEntity
+import scalaq.persistence.User
+
+
+case class HomeViewQuestionnaire(name: String, description: String, userId: Int, questId: Int, hash: Long, user: User, qOwner: QOwnerEntity) {
+
+  def changeActiveState() : Unit = {
+    PersistanceAdapter.changeActiveState(this)
+  }
+  
+
+}
